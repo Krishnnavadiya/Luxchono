104d61b218b96b4a57009fc0f8a966d0f853bf7a
princy7027 <princy702702@gmail.com>
2025-10-29 18:39:06 +0530

    chore: setup initial React project structure

---
+++ Luxchono-Admin-frontend/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme';
+import { store } from './Redux/store';
+import { Provider } from 'react-redux';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme} >
+      <App />
+    </ThemeProvider>
+  </Provider>
+
+
+);
+
+
+reportWebVitals();
