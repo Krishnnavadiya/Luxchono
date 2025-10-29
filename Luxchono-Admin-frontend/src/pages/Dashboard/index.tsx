d3c5f4897d7e4a89e5c7c21a012342aa3441e956
princy7027 <princy702702@gmail.com>
2025-10-29 19:29:45 +0530

    feat(product): integrate API to fetch and display product data

---
+++ Luxchono-Admin-frontend/src/pages/Dashboard/index.tsx
@@ -0,0 +1,19 @@
+import { Box, CssBaseline } from "@mui/material";
+import AppBars from "../../components/common/AppBar";
+import { DrawerHeader, Main } from "../../components/common/PageComonComponent/index";
+import Drawers from "../../components/common/Drawer";
+import DashboardPage from "../../components/Dashboard/DashboardPage";
+
+export default function Dashboard() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBars haddings="Dashboard" />
+      <Drawers />
+      <Main open={true}>
+        <DrawerHeader />
+        <DashboardPage/>
+      </Main>
+    </Box>
+  )
+}
