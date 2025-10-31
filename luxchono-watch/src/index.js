426f1e469142e4e17da3cbeb3e1c85294b9e2479
Vishva-lakkad07 <vishvalakkad07@gmail.com>
2025-10-31 13:06:31 +0530

    Initialize Luxchono Watch frontend with React and Tailwind configuration

---
+++ luxchono-watch/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+import "./index.css";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>
+);
+
+reportWebVitals();
