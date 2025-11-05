b1f1924a65bb5ebb352bbbb45bdbc1528751acb8
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-11-05 18:19:49 +0530

    feat(controller): implement addAddress method in address Controller

---
+++ luxchono-backend/src/controller/address_controller.js
@@ -0,0 +1,17 @@
+const ApiError = require("../util/error");
+const AddressModel = require("../model/address_model");
+
+async function addAddress(req, res, next) {
+    try {
+        const body = req.body;
+        body.uid = req.id;
+        const address = new AddressModel(body);
+        await address.save();
+        res.status(200).json({ statusCode: 200, success: true, message: 'Address save successfully' });
+    } catch (e) {
+        return next(new ApiError(400, 'Enter valid address details'));
+    }
+}
+
+
+module.exports = { addAddress, getAddress, getOneAddress, updateAddress, deleteAddress };
