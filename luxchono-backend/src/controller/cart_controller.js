af9c3411a2454f9f7ce67bf67d5b5843533a87f5
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-11-09 16:11:09 +0530

    feat(controller): add getAllCartProduct operations for cart modules

--- luxchono-backend/src/controller/cart_controller.js
+++ luxchono-backend/src/controller/cart_controller.js
@@ -37,5 +37,64 @@ async function addCart(req, res, next) {
     }
 }
 
+async function getAllCartProduct(req, res, next) {
+    try {
+        const id = req.id;
+        const cartProducts = await CartModel.aggregate([
+            {
+                $match: {
+                    uid: new mongoose.Types.ObjectId(id)
+                }
+            },
+            {
+                $lookup: {
+                    from: "products",
+                    foreignField: "_id",
+                    localField: "pid",
+                    as: "product",
+                    pipeline: [
+                        ...productPipeline
+                    ]
+                }
+            },
+            {
+                $project: {
+                    _id: "$_id",
+                    product: {
+                        $first: "$product"
+                    },
+                    quantity: "$quantity",
+                    createdAt: '$createdAt',
+                    updatedAt: "$updatedAt"
+                }
+            },
+            {
+                $addFields: {
+                    productTotalAmount: {
+                        $multiply: ["$quantity", "$product.dummyPrice"]
+                    },
+                    productDiscountAmount: {
+                        $subtract: [{ $multiply: ["$quantity", "$product.dummyPrice"] }, { $multiply: ["$quantity", "$product.price"] }]
+                    },
+                    productPaymentAmount: {
+                        $multiply: ["$quantity", "$product.price"]
+                    },
+                }
+            }
+        ]).exec();
+        let cartTotalAmount = 0;
+        let cartPaymentAmount = 0;
+        for (let i = 0; i < cartProducts.length; i++) {
+            cartPaymentAmount += cartProducts[i].productPaymentAmount;
+            cartTotalAmount += (cartProducts[i].product.dummyPrice * cartProducts[i].quantity);
+        }
+        let cartDiscountAmount = cartTotalAmount - cartPaymentAmount;
+        return res.status(200).json({ statusCode: 200, success: true, data: { cartProducts, cartTotalAmount, cartDiscountAmount, cartPaymentAmount } });
+    } catch (e) {
+        return next(new ApiError(400, e.message));
+    }
+}
+
+
 
 module.exports = { addCart, getAllCartProduct, updateCartProduct, removeCart, getAllCartIds };
