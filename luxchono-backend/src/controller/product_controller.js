00a2420d0cd201a15c84362e4ba0051e7b0cc3bc
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-11-09 18:29:38 +0530

    feat(controller): add getProduct operations for product modules

--- luxchono-backend/src/controller/product_controller.js
+++ luxchono-backend/src/controller/product_controller.js
@@ -65,6 +65,66 @@ const productPipeline = [
     }
 ];
 
+async function getProducts(req, res, next) {
+    try {
+        let pipeline = [];
+        const { category, brand, search, startPrice, endPrice, size } = req.query;
+
+        const filters = { isActive: true };
+
+        if (category && Array.isArray(category)) {
+            let categoryIds = [];
+            for (let i = 0; i < category.length; i++) {
+                categoryIds.push(new mongoose.Types.ObjectId(category[i]));
+            }
+            filters.category = { $in: categoryIds };
+        }
+
+        if (brand && Array.isArray(brand)) {
+            let brandIds = [];
+            for (let i = 0; i < brand.length; i++) {
+                brandIds.push(new mongoose.Types.ObjectId(brand[i]));
+            }
+            filters.brand = { $in: brandIds };
+        }
+
+        if (startPrice && !isNaN(startPrice)) {
+            filters.price = { $gte: parseFloat(startPrice) };
+        }
+
+        if (endPrice && !isNaN(endPrice)) {
+            filters.price = filters.price || {};
+            filters.price.$lte = parseFloat(endPrice);
+        }
+
+        if (search) {
+            filters.$or = [
+                { name: { $regex: new RegExp(search, 'i') } },
+                { productModel: { $regex: new RegExp(search, 'i') } },
+            ];
+        }
+        pipeline.push(...productPipeline);
+        if (size) {
+            pipeline.push(
+                {
+                    $sample: { size: parseInt(size) }
+                }
+            );
+        }
+        if (filters) {
+            pipeline.unshift(
+                {
+                    $match: filters
+                }
+            )
+        }
+        const products = await ProductModel.aggregate(pipeline).exec();
+        res.status(200).json({ statusCode: 200, success: true, data: products });
+    } catch (e) {
+        return next(new ApiError(400, e.message));
+    }
+}
+
 
 
 module.exports = { getProducts, productPipeline, getOneProduct };
