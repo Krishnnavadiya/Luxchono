6b096ec891e88653ac954a50eea2264f870299b9
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-11-09 18:19:06 +0530

    feat(controller): add addRemoveLike operations for like modules

--- luxchono-backend/src/controller/like_controller.js
+++ luxchono-backend/src/controller/like_controller.js
@@ -37,36 +37,6 @@ async function addRemoveLike(req, res, next) {
     }
 }
 
-async function getAllLikeProduct(req, res, next) {
-    try {
-        const id = req.id;
-        const likeModels = await LikeModel.find({ uid: id }).select({ pid: true });
-        const ids = likeModels.map((e) => e.pid);
-        const likeProducts = await ProductModel.aggregate([
-            {
-                $match: {
-                    _id: {
-                        $in: ids
-                    }
-                }
-            },
-            ...productPipeline
-        ]).exec();
-        return res.status(200).json({ statusCode: 200, success: true, data: likeProducts });
-    } catch (e) {
-        return next(new ApiError(400, e.message));
-    }
-}
 
-async function getAllLikeIds(req, res, next) {
-    try {
-        const id = req.id;
-        const likeModels = await LikeModel.find({ uid: id }).select({ pid: true });
-        const ids = likeModels.map((e) => e.pid);
-        return res.status(200).json({ statusCode: 200, success: true, data: ids });
-    } catch (e) {
-        return next(new ApiError(400, e.message));
-    }
-}
 
 module.exports = { addRemoveLike, getAllLikeProduct, getAllLikeIds };
