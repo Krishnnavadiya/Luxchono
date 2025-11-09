9781882e700171815e3385bfe78c0120766fa272
Krishnnavadiya <139139071+Krishnnavadiya@users.noreply.github.com>
2025-11-09 18:29:58 +0530

    feat(controller): add getOneProduct operations for product modules

--- luxchono-backend/src/controller/product_controller.js
+++ luxchono-backend/src/controller/product_controller.js
@@ -125,6 +125,53 @@ async function getProducts(req, res, next) {
     }
 }
 
-
+async function getOneProduct(req, res, next) {
+    try {
+        const id = req.params.id;
+        const isValid = mongoose.isValidObjectId(id);
+        if (!isValid) {
+            return next(new ApiError(400, "Id is not valid"));
+        }
+        const products = await ProductModel.aggregate([
+            {
+                $match: {
+                    _id: new mongoose.Types.ObjectId(id),
+                    isActive: true,
+                }
+            },
+            ...productPipeline
+        ]).exec();
+        if (products.length === 0) {
+            return next(new ApiError(400, "Product is not exist"));
+        }
+        const similarProduct = await ProductModel.aggregate([
+            {
+                $match: {
+                    isActive: true,
+                    $and: [
+                        { _id: { $ne: products[0]._id } },
+                        {
+                            $or: [
+                                { category: { $in: products[0].category.map((e) => e._id) } },
+                                { brand: { $eq: products[0].brand._id } }
+                            ]
+                        }
+                    ]
+                }
+            },
+            ...productPipeline,
+            {
+                $limit: 8
+            }
+        ]).exec()
+        const ratings = await RatingModel.find({ product: products[0]._id }).populate({
+            path: "user",
+            select: "email username"
+        });
+        res.status(200).json({ statusCode: 200, success: true, data: { product: products[0], similarProduct, ratings }, message: "Get one product successfully" });
+    } catch (e) {
+        return next(new ApiError(400, e.message));
+    }
+}
 
 module.exports = { getProducts, productPipeline, getOneProduct };
