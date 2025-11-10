8a031967937f7678ac42427d5cfcb35040815cca
princy7027 <princy702702@gmail.com>
2025-11-10 19:55:00 +0530

    feat(offer): add component for add offer page

---
+++ Luxchono-Admin-frontend/src/pages/Offer/AddOfferPage/index.tsx
@@ -0,0 +1,20 @@
+import { Box, CssBaseline } from "@mui/material";
+import AppBars from "../../../components/common/AppBar/index";
+import { DrawerHeader, Main } from "../../../components/common/PageComonComponent/index";
+import Drawers from "../../../components/common/Drawer";
+import AddOfferPage from "../../../components/Offer/AddOfferPage";
+
+
+export default function AddOffer() {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBars haddings="Offer" />
+            <Drawers />
+            <Main open={true}  >
+                <DrawerHeader />
+                <AddOfferPage />
+            </Main>
+        </Box>
+    )
+}
