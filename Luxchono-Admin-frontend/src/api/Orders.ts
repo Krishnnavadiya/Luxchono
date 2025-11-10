eaca414ad8ac250e49604a421463a25240fd9ef5
princy7027 <princy702702@gmail.com>
2025-11-10 19:52:35 +0530

    feat(order): add fetch API for orders and order details

---
+++ Luxchono-Admin-frontend/src/api/Orders.ts
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { prepareHeaders } from "./Utils";
+import queryString from 'query-string';
+
+export const OrdersApi = createApi({
+    reducerPath: 'OrdersApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.REACT_APP_BASE_URL,
+        prepareHeaders: prepareHeaders,
+        paramsSerializer: function (params: any) {
+            return queryString.stringify(params, { arrayFormat: 'index' });
+        },
+    }),
+    tagTypes: ["Orders"],
+    endpoints: (builder) => ({
+        GetAllOrders: builder.query({
+            query: () => {
+                return {
+                    url: '/admin/order/',
+                    method: "GET",
+                };
+            },
+            providesTags: ["Orders"],
+        }),
+
+        UpdateOrderStatus: builder.mutation({
+            query: (body) => {
+                return {
+                    url: `/admin/order/status-change`,
+                    method: 'PATCH',
+                    body
+                };
+            },
+            invalidatesTags: ['Orders'],
+        })
+
+    }),
+});
+
+export const { useGetAllOrdersQuery,useUpdateOrderStatusMutation } = OrdersApi;
